ca88ffa0112882ca677ec1372eb31a79e858e98b
Bashra99 <telfahb@gmail.com>
2022-10-31 20:20:05 +0000

    feat:dealing with API to get country name and its capital

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -9,20 +9,23 @@ class handler(BaseHTTPRequestHandler):
        url_components = parse.urlsplit(s)
        query_string_list = parse.parse_qsl(url_components.query)
        dictionary = dict(query_string_list)
-       if country in dictionary:
+
+       if 'country' in dictionary:
             country = dictionary['country']
-            url=f'https://restcountries.eu/rest/v2/name/{country}'
+            url=f'https://restcountries.eu/rest/v3.1/name/{country}'
             response = requests.get(url)
             data = response.json()
             capital = data[0]['capital']
             massage=f'The capital of {country} is {capital}.'
-       elif capital in dictionary:
+       elif 'capital' in dictionary:
             capital = dictionary['capital']
-            url=f'https://restcountries.eu/rest/v2/capital/{capital}'
+            url=f'https://restcountries.eu/rest/v3.1/capital/{capital}'
             response = requests.get(url)
             data = response.json()
             country = data[0]['name']
             massage=f'{capital} is the capital of {country}.'
+       else:
+           massage="please enter country or capital"
 
 
 
